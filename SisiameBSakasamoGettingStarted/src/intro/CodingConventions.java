16dd009dde4e08de5b4a5e40c81b46086478ce3d
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-09-12 11:45:42 -0400

    Added to IntroMain and CodingConventions

--- SisiameBSakasamoGettingStarted/src/intro/CodingConventions.java
+++ SisiameBSakasamoGettingStarted/src/intro/CodingConventions.java
@@ -3,6 +3,10 @@ package intro;
 //class is a file describing a new data type
 public class CodingConventions {
 	
+	//fields (has-a relationships)
+	private String name;
+	private String description;
+	
 	/*
 	 * this is a special method, a "constructor"
 	 * notice it does not have a return type
@@ -10,7 +14,9 @@ public class CodingConventions {
 	 * therefore the name of a constructor must always
 	 * match the name of the class*/
 	public CodingConventions() {
-		
+		//in Java, fields are instantiated in a constructor
+		name = "Mr. Nockles";
+		description = " is a teacher";
 	}
 	
 }
--- SisiameBSakasamoGettingStarted/src/intro/IntroMain.java
+++ SisiameBSakasamoGettingStarted/src/intro/IntroMain.java
@@ -1,9 +1,15 @@
-package intro;
+package intro; //this is a package declaration
 
+//this is a class header (class declaration)
 public class IntroMain {
 
 	public static void main(String[] args) {
-
+		
+		/*declaration of a local variable
+		 * notice the use of the word 'new'
+		 * 'new' must be used to call a constructor*/
+		CodingConventions conventionsInstance = new CodingConventions();
+		
 	}
 
 }
