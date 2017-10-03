0eef8ab03e8b67dc222ee333f6e0e75993b0c8bb
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-10-03 11:48:00 -0400

    Lots of work on the conversation, mostly trigger words and input-dependent events

--- Chatbot Program/src/chatbot/ChatSisi.java
+++ Chatbot Program/src/chatbot/ChatSisi.java
@@ -1,11 +1,13 @@
 package chatbot;
 
 public class ChatSisi implements Topic {
-
+	
 	private String[] keywords;
 	private String goodbyeWord;
-	private String secretWord;
+	private String[] triggerWords;
+	private String[] previousTriggerWords;
 	private boolean chatting;
+	private int irritationLevel;
 	
 	public ChatSisi() {
 		
@@ -13,7 +15,11 @@ public class ChatSisi implements Topic {
 						 "noun", "verb", "adjective", "adverb", "pronoun", "proper noun"};
 		keywords = temp;
 		goodbyeWord = "i give up";
-		secretWord = "pug";
+		String[] temp2 = {"they're are", "they're were", "they're is", "their were", "their are", 
+						  "their is", "over their", "over they're", "over their", "that's theres", 
+						  "it's theres", "that's there's", "it's there's"};
+		triggerWords = temp2;
+		irritationLevel = 0;
 		
 	}
 	
@@ -46,11 +52,110 @@ public class ChatSisi implements Topic {
 				ChatbotMain.chatbot.startTalking();
 			} else {
 				
-				if(ChatbotMain.findKeyword(response, secretWord, 0) >= 0) {
-					ChatbotMain.print("Oh my goodness! You guessed my favorite thing ever. We are friends now.");
-				} else {
-					ChatbotMain.print("Huh, I don't really get you. Tell me something else?");
+				for(int i = 0; i < triggerWords.length; i++) {
+				
+					if(ChatbotMain.findKeyword(response, triggerWords[i], 0) >= 0) {
+						
+						if(irritationLevel < 2) {
+							ChatbotMain.print("Stop. You just said '" + triggerWords[i] + "'. That's not correct. Let me explain it.");
+							ChatbotMain.print("There are 3 different words that sound almost exactly the same: there, their, and they're. "
+									+ "They are very commonly used incorrectly. But the words are all very different.");
+							ChatbotMain.print("'There' can either be used as an adverb, as in 'there it is', an existential clause, "
+									+ "as in 'there is no cake', or an exclamation, as in 'there, I told you so!'");
+							ChatbotMain.print("'Their' is a determiner. It is either used to show ownership, as in 'that is their room', "
+									+ "or in titles, as in 'Their Majesty.'");
+							ChatbotMain.print("'They're' is simply a contraction of the term 'they are'. So you would use it as you would "
+									+ "use 'they are'. For example, 'they're coming'.");
+							ChatbotMain.print("I hope you understand better now.");
+						} else {
+							
+							if(irritationLevel < 3) {
+								ChatbotMain.print("Stop, stop. Let me explain. Again. You just said '" + triggerWords[i] + "'. That is INCORRECT.");
+								ChatbotMain.print("There is either an adverb, an existential clause, or an exclamation. "
+										+ "Their is a determiner. "
+										+ "They're is a contraction of the words 'they' and 'are'.");
+								ChatbotMain.print("An example of all 3 being used together correctly is 'they're on their way there'.");
+								ChatbotMain.print("Do you understand now?");
+							} else {
+								
+								if(irritationLevel < 4) {
+									ChatbotMain.print("Okay, just stop. You said '" + triggerWords[i] + "'. I know you need help, but please at "
+											+ "least listen to what I say. It really, REALLY sets me off when people use the wrong word of those 3.");
+									ChatbotMain.print("Let me try to help you out there.");
+									
+									for(int n = 0; n < previousTriggerWords.length; n++) {
+										
+										if(ChatbotMain.findKeyword(previousTriggerWords[i], "there", 0) >= 0) {
+											ChatbotMain.print("You used the word '" + previousTriggerWords[n] + "'. The correct word to use here would've "
+													+ "been 'their'. This is because what you said was meant to express ownership of some sort.");
+										} else {
+											ChatbotMain.print("You used the word '" + previousTriggerWords[n] + "'. The correct word to use here would've "
+													+ "been 'there'. This is because what you said was meant to express either the location of something, "
+													+ "or the existence of something.");
+										}
+										
+									}
+									
+									ChatbotMain.print("And now you used the word '" + triggerWords[i] + "'. That is also incorrect.");
+									
+									if(ChatbotMain.findKeyword(triggerWords[i], "there", 0) >= 0) {
+										ChatbotMain.print("The correct word to use here would've been 'their'. This is because what you said was meant "
+												+ "to express ownership of some sort.");
+									} else {
+										ChatbotMain.print("The correct word to use here would've been 'there'. This is because what you said was meant to "
+												+ "express either the location of something, or the existence of something.");
+									}
+									
+									ChatbotMain.print("Now, do you finally understand?");
+									
+								} else {
+								
+									if(irritationLevel < 5) {
+										ChatbotMain.print("Listen. Stop it. You just said '" + triggerWords[i] + "'. We went over this. I'm about to reach "
+												+ "my breaking point. You know what's wrong. Don't bother me again.");
+									} else {
+										
+										ChatbotMain.print("Okay, I've had it up to here. I'm done. Go ask Google for help or something. It's where I'm getting "
+												+ "all this info from anyway. Here are all your silly mistakes for you to look up: ");
+										
+										for(int n = 0; n < previousTriggerWords.length; n++) {
+											
+											ChatbotMain.print(previousTriggerWords[n] + ", ");
+											
+										}
+										
+										ChatbotMain.print(triggerWords[i] + ".");
+										ChatbotMain.print("Good riddance.");
+										
+										chatting = false;
+										ChatbotMain.chatbot.startTalking();
+										
+									}
+									
+								}
+								
+							}
+							
+						}
+						
+						if(previousTriggerWords.length > 0) {
+						
+							if(previousTriggerWords[previousTriggerWords.length - 1] == triggerWords[i]) {
+								irritationLevel += 10;
+							} else {
+							irritationLevel++;
+							}
+							
+						}
+						
+						previousTriggerWords[previousTriggerWords.length - 1] = triggerWords[i];
+						return;
+							
+					}
+					
 				}
+				
+				ChatbotMain.print("I do not comprehend what you are saying. My intelligence is limited. Say something else.");
 				
 			}
 			
