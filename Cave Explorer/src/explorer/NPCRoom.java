a10a91e23e5a8efbfac27f2f2dc7b3f08af9eaab
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-10-31 10:37:13 -0400

    NPC classes added

---
+++ Cave Explorer/src/explorer/NPCRoom.java
@@ -0,0 +1,44 @@
+package explorer;
+
+import entity.NPC;
+
+public class NPCRoom extends CaveRoom {
+	
+	private NPC presentNPC;
+	
+	public NPCRoom(String description) {
+		
+		super(description);
+		presentNPC = null;
+		
+	}
+	
+	/*
+	 * NPCs can enter a room if no other NPC is in the room
+	 * @return
+	 */
+	public boolean canEnter() {
+		
+		return presentNPC == null;
+		
+	}
+	
+	public void enterNPC(NPC m) {
+		
+		presentNPC = m;
+		
+	}
+	
+	public void leaveNPC() {
+		
+		presentNPC = null;
+		
+	}
+	
+	public boolean containsNPC() {
+		
+		return presentNPC != null;
+		
+	}
+	
+}
