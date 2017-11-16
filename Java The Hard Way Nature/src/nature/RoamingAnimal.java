ff1b6562adc4da223145a6f29da59a4b4d942665
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-11-16 10:47:10 -0500

    All questions placed in files

--- Java The Hard Way Nature/src/nature/RoamingAnimal.java
+++ Java The Hard Way Nature/src/nature/RoamingAnimal.java
@@ -5,3 +5,14 @@ public class RoamingAnimal extends Animal {
 	
 	
 }
+
+/*
+1. What is the point of a mate() method at line 43 if the method just prints "does not have a mating ritual"?
+ *
+2. In line 31, why is it necessary to check whether a is null? When would a ever be null?
+ *
+3. There is no boolean instance variable "living" to keep track of whether an animal is alive or dead. In lines 50 and 55, animals do die. How does the program "keep track" of which animals are alive and which animals are dead?
+ *
+4. Under what circumstances would an animal ever die of starvation? Can you explain how the method at line 64 works?
+ *
+*/
